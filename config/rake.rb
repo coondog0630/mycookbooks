be7047d05e310c3084ab9c6d4421a8db815b9bd3
Sahil R Cooner <sahil.cooner@gmail.com>
2010-06-27 15:24:18 -0700

    added my locale info
    Signed-off-by: Sahil R Cooner <sahil.cooner@gmail.com>

--- config/rake.rb
+++ config/rake.rb
@@ -6,22 +6,22 @@
 ###
 
 # The company name - used for SSL certificates, and in srvious other places
-COMPANY_NAME = "Example Com"
+COMPANY_NAME = "Sahil Cooner DOT Com"
 
 # The Country Name to use for SSL Certificates
 SSL_COUNTRY_NAME = "US"
 
 # The State Name to use for SSL Certificates
-SSL_STATE_NAME = "Several"
+SSL_STATE_NAME = "CA"
 
 # The Locality Name for SSL - typically, the city
-SSL_LOCALITY_NAME = "Locality"
+SSL_LOCALITY_NAME = "San Francisco"
 
 # What department?
 SSL_ORGANIZATIONAL_UNIT_NAME = "Operations"
 
 # The SSL contact email address
-SSL_EMAIL_ADDRESS = "ops@example.com"
+SSL_EMAIL_ADDRESS = "ops@sahilcooner.com"
 
 # License for new Cookbooks
 # Can be :apachev2 or :none
